d79bfbeeb02aca3adc90d3f10515f01ecf2082c1
thigarette <thigamureithi@hotmail.com>
2018-07-04 02:52:33 +0300

    MapsActivity Added

--- AfroFitnessAndroid/app/src/main/java/com/example/thiga/afrofitness_android/ui/HomeActivity.java
+++ AfroFitnessAndroid/app/src/main/java/com/example/thiga/afrofitness_android/ui/HomeActivity.java
@@ -49,8 +49,9 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(),MapsActivity.class));
             }
         });
 
